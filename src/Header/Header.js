f82997496fd967e68217dafa79032c713b4015a2
Ummer Khan <ummernkhan@gmail.com>
2023-06-04 13:21:03 -0700

    adds splash image

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,14 +1,51 @@
 import React, { Component } from 'react';
 import './Header.css';
 
-class Header extends Component {
-  render() {
-    return (
-      <div className="header">
-        Header
-      </div>
-    );
-  }
+// class Header extends Component {
+//   render() {
+//     return (
+//       <div className="header">
+//         Header
+//       </div>
+//     );
+//   }
+// }
+
+// export default Header;
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function Header() {
+  return (
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Law Office of Khan-Avila</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">About</Nav.Link>
+              <Nav.Link href="#link">Contact</Nav.Link>
+              <NavDropdown title="Services" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1"></NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <img id="splash" src="../imgs/splash.png"/>
+    </div>
+  );
 }
 
 export default Header;
