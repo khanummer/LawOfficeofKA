5ce883f42f9b0415935c82b6101a03643fe6fe49
Ummer Khan <ummernkhan@gmail.com>
2023-06-09 01:19:51 -0700

    edits MTT text, adds header to Services

--- src/MTT/MTT.js
+++ src/MTT/MTT.js
@@ -3,23 +3,23 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import './MTT.css';
+import Image from 'react-bootstrap/Image';
 
 function MTT() {
     return (
-
-    
+    <div>
+        <h1>Meet The Team</h1>
         <Container>
             <Row>
-                <Col xs={12} md={8}>
-                     xs=12 md=8
+                <Col xs={12} md={6}>
+                <Image src="../imgs/splash.png" className="headshot"/>
                 </Col>
-
-                <Col xs={6} md={4}>
-                    xs=6 md=4
+                <Col xs={12} md={6}>
+                    <div className="mtt-text">Maleha Khan-Avila</div>
                 </Col>
             </Row>
         </Container>
-    
+    </div>
 
     )
 };
--- src/Services/Services.js
+++ src/Services/Services.js
@@ -3,27 +3,35 @@ import './Services.css';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 
 function Services() {
   return (
-    <Row xs={1} md={2} className="g-4">
-      {Array.from({ length: 4 }).map((_, idx) => (
-        <Col key={idx}>
-          <Card>
-            <Card.Img variant="top" src="../imgs/splash.png" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      ))}
-    </Row>
+    <div>
+      <h1>Services</h1>
+      <Container className="services">
+        <Row xs={1} md={2} lg={4} className="g-4">
+          {Array.from({ length: 4 }).map((_, idx) => (
+            <Col key={idx}>
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="../imgs/splash.png" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and make up the
+                      bulk of the card's content.
+                    </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </div>
   );
 }
 
 export default Services;
+
